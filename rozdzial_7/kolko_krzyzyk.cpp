ffbfc826d9670b5fe9cdfc91fdc1b2aa456dba5d
sczembor <stanislaw.czembor@gmail.com>
2018-10-18 22:05:37 +0200

    poprawki graficzne gry tic-tac-toe

--- rozdzial_7/kolko_krzyzyk.cpp
+++ rozdzial_7/kolko_krzyzyk.cpp
@@ -10,20 +10,16 @@
 
 using namespace std;
 
+char plansza[3][3] ={'1','2','3','4','5','6','7','8','9'};
+void pole();
+char ruch2;
+char ruch1;
+
 int main()
 {
-    char plansza[3][3] ={'1','2','3','4','5','6','7','8','9'};
-    char ruch2;
-    char ruch1;
-    for(int k=0;k<3;k++)
-    {
-        for(int i = 0;i<3; i++)
-        {
-            cout<<plansza[k][i]<<" ";
-        }
-        cout<<endl;
-        
-    }
+    
+    
+    pole();
     for(int a = 1; a<10;a++){
         if(a%2==0){
             cout<<"player's 2 turn (X) : ";
@@ -57,68 +53,65 @@ int main()
                     plansza[2][2] = 'X';
                     break;
             }
-                for(int k=0;k<3;k++)
-                {
-                    for(int i = 0;i<3; i++)
-                    {
-                        cout<<plansza[k][i]<<" ";
-                    }
-                    cout<<endl;
-                    
-                }}
+            pole();
+        }
         else
-                    {
-                        cout<<"player's 1 turn (O) : ";
-                        cin>>ruch1;
-                        switch(ruch1){
-                            case '1':
-                                plansza[0][0] = 'O';
-                                break;
-                            case '2':
-                                plansza[0][1] = 'O';
-                                break;
-                            case '3':
-                                plansza[0][2] = 'O';
-                                break;
-                            case '4':
-                                plansza[1][0] = 'O';
-                                break;
-                            case '5':
-                                plansza[1][1] = 'O';
-                                break;
-                            case '6':
-                                plansza[1][2] = 'O';
-                                break;
-                            case '7':
-                                plansza[2][0] = 'O';
-                                break;
-                            case '8':
-                                plansza[2][1] = 'O';
-                                break;
-                            case '9':
-                                plansza[2][2] = 'O';
-                                break;
-                        }
-                                for(int k=0;k<3;k++)
-                                {
-                                    for(int i = 0;i<3; i++)
-                                    {
-                                        cout<<plansza[k][i]<<" ";
-                                    }
-                                    cout<<endl;
+        {
+            cout<<"player's 1 turn (O) : ";
+            cin>>ruch1;
+            switch(ruch1){
+                case '1':
+                    plansza[0][0] = 'O';
+                    break;
+                case '2':
+                    plansza[0][1] = 'O';
+                    break;
+                case '3':
+                    plansza[0][2] = 'O';
+                    break;
+                case '4':
+                    plansza[1][0] = 'O';
+                    break;
+                case '5':
+                    plansza[1][1] = 'O';
+                    break;
+                case '6':
+                    plansza[1][2] = 'O';
+                    break;
+                case '7':
+                    plansza[2][0] = 'O';
+                    break;
+                case '8':
+                    plansza[2][1] = 'O';
+                    break;
+                case '9':
+                    plansza[2][2] = 'O';
+                    break;
+            }
+            pole();
             
         }
-            
-        }
-            
+        
         
     }
-
     
+}
+void pole()
+{
+    cout<<"     |     |     "<<endl;
+    cout<<"  "<<plansza[0][0]<<"  |"<<"  "<<plansza[0][1]<<"  |"<<"  "<<plansza[0][2]<<"   "<<endl;
+    cout<<"_____|_____|_____"<<endl;
+    cout<<"     |     |     "<<endl;
+    cout<<"  "<<plansza[1][0]<<"  |"<<"  "<<plansza[1][1]<<"  |"<<"  "<<plansza[1][2]<<"   "<<endl;
+    cout<<"_____|_____|_____"<<endl;
+    cout<<"     |     |     "<<endl;
+    cout<<"  "<<plansza[2][0]<<"  |"<<"  "<<plansza[2][1]<<"  |"<<"  "<<plansza[2][2]<<"   "<<endl;
+    cout<<"     |     |     "<<endl<<endl<<endl<<endl<<endl<<endl;
     
 }
 
- 
-    
-    
+
+
+
+
 
